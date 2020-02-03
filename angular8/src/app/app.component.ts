e9afe387be0be5136c4bbc1e9f2c0a9b91760832
Linus Gasser <linus.gasser@epfl.ch>
2020-02-03 17:32:07 +0100

    Short demo of crypto-browserify, dynacred, and fetching the latest block from the DEDIS blockchain

--- angular8/src/app/app.component.ts
+++ angular8/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Log} from "@dedis/cothority";
+import {ByzCoinRPC} from "@dedis/cothority/byzcoin";
+import {WebSocketConnection} from "@dedis/cothority/network/connection";
+import {randomBytes} from "crypto-browserify";
+import {KeyPair} from "@c4dt/dynacred";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular8';
+
+  async ngOnInit(){
+    // Print 32 random bytes
+    Log.print(randomBytes(32));
+
+    // Create a new random keypair from the dynacred library
+    Log.print(new KeyPair());
+
+    // Get the latest block from the DEDIS byzcoin
+    const conn = new WebSocketConnection("wss://conode.c4dt.org:7771", "byzcoin");
+    const bc = await ByzCoinRPC.fromByzcoin(conn, Buffer.from("9cc36071ccb902a1de7e0d21a2c176d73894b1cf88ae4cc2ba4c95cd76f474f3", "hex"));
+    Log.print(bc.getConfig());
+  }
+
 }
--- nativescript6/app/main-view-model.ts
+++ nativescript6/app/main-view-model.ts
@@ -1,4 +1,13 @@
 import { Observable } from "tns-core-modules/data/observable";
+import {randomBytes} from "crypto-browserify";
+import {Log} from "@dedis/cothority";
+import {ByzCoinRPC} from "@dedis/cothority/byzcoin";
+import {
+    WebSocketConnection
+} from "@dedis/cothority/network/connection";
+import {WebSocketAdapter} from "@dedis/cothority/network";
+import {KeyPair} from "@c4dt/dynacred";
+import {NativescriptWebSocketAdapter} from "~/nativescript-ws";
 
 export class HelloWorldModel extends Observable {
 
@@ -24,7 +33,19 @@ export class HelloWorldModel extends Observable {
         }
     }
 
-    onTap() {
+    async onTap() {
+        // Print 32 bytes of randomness
+        Log.print(randomBytes(32));
+
+        // Create a new random keypair from the dynacred library
+        Log.print(new KeyPair());
+
+        // Set websockets to nativescript-websockets and fetch the latest
+        // block from the DEDIS-blockchain
+        const conn = new WebSocketConnection("wss://conode.c4dt.org:7771", "byzcoin");
+        const bc = await ByzCoinRPC.fromByzcoin(conn, Buffer.from("9cc36071ccb902a1de7e0d21a2c176d73894b1cf88ae4cc2ba4c95cd76f474f3", "hex"));
+        Log.print(bc.getConfig());
+
         this._counter--;
         this.updateMessage();
     }
